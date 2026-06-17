f469b04c41ef5a5daafb5ba8f770aa373cf925c0
muse <miaoyulun380@gmail.com>
2026-06-17 10:04:34 +0100

    finish Logger

---
+++ utils/logger.py
@@ -0,0 +1,24 @@
+import logging
+
+def get_logger(name):
+    # 1. create logger
+    logger = logging.getLogger(name)
+
+    # 2. create level
+    logger.setLevel(logging.INFO)
+
+    if not logger.hanlders:
+        # 3. create handler
+        handler = logging.StreamHandler()
+
+        # 4. create format
+        formatter = logging.Formatter(
+            "%(asctime)s | %(levelname)-8s | %(name)s | %(message)s"
+        )
+
+        # 5. assembly
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        
+    return logger
+
